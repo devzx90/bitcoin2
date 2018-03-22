737792825d38d630f22def0444c89c2a639158b4
BTC2CoreDevs <20080595+BTC2CoreDevs@users.noreply.github.com>
2018-03-22 13:03:25 +0700

    -

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -1,4 +1,5 @@
-# Copyright (c) 2016 The bitcoin2 Core developers
+# Copyright (c) 2016 The Bitcoin Core developers
+# Copyright (c) 2017 - 2018 Bitcoin 2 Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
