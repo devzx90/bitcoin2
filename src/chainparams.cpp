4d9426f50bfe937d4459aaec6850950514e225d4
BTC2CoreDevs <20080595+BTC2CoreDevs@users.noreply.github.com>
2018-03-26 18:42:50 +0700

    -

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -262,9 +262,9 @@ public:
 		base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 239);
 		base58Prefixes[EXT_PUBLIC_KEY] = { 0x04, 0x35, 0x87, 0xCF };
 		base58Prefixes[EXT_SECRET_KEY] = { 0x04, 0x35, 0x83, 0x94 };
-        //convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
+        convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
 
-        fMiningRequiresPeers = false; // TODO: CHANGE
+        fMiningRequiresPeers = true;
         fAllowMinDifficultyBlocks = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = false;
