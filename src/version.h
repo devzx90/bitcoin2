3459946043585d53db93968ad912fbd0c5b21c91
Hiromono <38851916+Hiromono@users.noreply.github.com>
2020-09-03 08:59:17 +0300

    Define new protocol and upgrade height

--- src/version.h
+++ src/version.h
@@ -8,34 +8,36 @@
 #ifndef BITCOIN_VERSION_H
 #define BITCOIN_VERSION_H
 
+const int REWARDFORK_BLOCK = 1141660; // Introduced for protocol 70914
+
 /**
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70913;
+const int PROTOCOL_VERSION = 70914;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70912;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70913;
+const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70913;
+const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70914;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+const int CADDR_TIME_VERSION = 31402;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+const int BIP0031_VERSION = 60000;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
+const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70005;
+const int NO_BLOOM_VERSION = 70005;
 
 
 #endif // BITCOIN_VERSION_H
